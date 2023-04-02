62e3dd0cd95e7f5a477b87f74017d4f33b2fd5c5
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-02 14:25:35 +0900

    画面作成

--- RxSwiftSampleApp/ViewController.swift
+++ RxSwiftSampleApp/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var button: UIButton!
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.rx.tap.subscribe(onNext: { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.label.text = "押されたぁぁぁ"
+        })
+        .disposed(by: disposeBag)
+        
+        Observable
+            .combineLatest(textField1.rx.text.orEmpty,
+                           textField2.rx.text.orEmpty)
+            .subscribe(onNext: { [weak self] text1, text2 in
+                guard let strongSelf = self else { return }
+                strongSelf.button.isHidden = !strongSelf.isValid(text1: text1, text2: text2)
+            })
+            .disposed(by: disposeBag)
     }
-
-
+    
+    private func isValid(text1: String, text2: String) -> Bool {
+        text1.count > 2 &&  text2.count > 2 && text1 == text2
+    }
 }
 
